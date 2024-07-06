cf90c4b00d1192cdbdb4bc192c8744bcd8051c64
danradchuk <radchuk@fastmail.com>
2024-07-06 19:58:08 +0200

    refactor

--- geometry/shapes.go
+++ geometry/shapes.go
@@ -19,88 +19,118 @@ type HitRecord struct {
 }
 
 // Sphere
-// type Sphere struct {
-// 	Center   Vec3
-// 	R        float64
-// 	Material shading.Material
-// }
-//
-// func (s Sphere) GetMaterial() shading.Material {
-// 	return s.Material
-// }
-//
-// func (s Sphere) Intersection(r Ray) (bool, float64) {
-// 	co := r.Origin.Sub(s.Center)
-//
-// 	a := r.Direction.Dot(r.Direction)
-// 	b := 2.0 * co.Dot(r.Direction)
-// 	c := co.Dot(co) - s.R*s.R
-//
-// 	d := b*b - 4.0*a*c
-// 	if d < 0 {
-// 		return false, 0 // no intersection
-// 	}
-//
-// 	t1 := (-b + math.Sqrt(d)) / (2.0 * a)
-// 	t2 := (-b - math.Sqrt(d)) / (2.0 * a)
-//
-// 	return true, math.Min(t1, t2)
-// }
-//
-// func (s Sphere) NormalAt(p Vec3) Vec3 {
-// 	return p.Sub(s.Center).Normalize()
-// }
+type Sphere struct {
+	Center   Vec3
+	R        float64
+	Material shading.Material
+}
+
+func (s Sphere) Intersect(r Ray) *HitRecord {
+	co := r.Origin.Sub(s.Center)
+
+	a := r.Direction.Dot(r.Direction)
+	b := 2.0 * co.Dot(r.Direction)
+	c := co.Dot(co) - s.R*s.R
+
+	d := b*b - 4.0*a*c
+	if d < 0 {
+		return nil // no intersection
+	}
+
+	t1 := (-b + math.Sqrt(d)) / (2.0 * a)
+	t2 := (-b - math.Sqrt(d)) / (2.0 * a)
+
+	tMin := math.Min(t1, t2)
+
+	p := r.At(tMin)
+
+	return &HitRecord{tMin, s, s.Material, p.Sub(s.Center).Normalize()}
+}
+
+func (s Sphere) Bounds() Bounds3 {
+	center := s.Center
+	radius := s.R
+
+	pMin := Point3{center.X - radius, center.Y - radius, center.Z - radius}
+	pMax := Point3{center.X + radius, center.Y + radius, center.Z + radius}
+
+	return Bounds3{pMin, pMax}
+}
 
 // Plane
-// type Plane struct {
-// 	Width    float64
-// 	Point    Vec3
-// 	Normal   Vec3
-// 	Material shading.Material
-// }
-//
-// func (p Plane) GetMaterial() shading.Material {
-// 	return p.Material
-// }
-//
-// func (p Plane) Intersection(r Ray) *HitRecord {
-// 	// t = ((p0 - l0) * n) / (l * n)
-// 	// p0 - point on the plane
-// 	// l0 - origin of the ray
-// 	// n - normal
-// 	// l - ray direction
-//
-// 	n := p.Normal
-// 	denom := n.Dot(r.Direction) // l * n
-// 	if math.Abs(denom) < 1e-6 {
-// 		return nil
-// 	}
-//
-// 	p0l0 := p.Point.Sub(r.Origin) // p0 - l0
-// 	t := p0l0.Dot(n) / denom
-// 	if t < 0 {
-// 		return nil
-// 	}
-//
-// 	xMin := p.Point.X - (p.Width / 2)
-// 	xMax := p.Point.X + (p.Width / 2)
-// 	zMin := p.Point.Z - (p.Width / 2)
-// 	zMax := p.Point.Z + (p.Width / 2)
-//
-// 	ir := r.At(t)
-// 	if ir.X >= xMin && ir.X <= xMax && ir.Z >= zMin && ir.Z <= zMax {
-// 		return &HitRecord{
-// 			T:         t,
-// 			Primitive: &p,
-// 		}
-// 	}
-//
-// 	return nil
-// }
-//
-// func (p Plane) NormalAt(_ Vec3) Vec3 {
-// 	return p.Normal.Normalize()
-// }
+type Plane struct {
+	Width    float64
+	Point    Vec3
+	Normal   Vec3
+	Material shading.Material
+}
+
+func (p Plane) Intersect(r Ray) *HitRecord {
+	// t = ((p0 - l0) * n) / (l * n)
+	// p0 - point on the plane
+	// l0 - origin of the ray
+	// n - normal
+	// l - ray direction
+
+	n := p.Normal
+	denom := n.Dot(r.Direction) // l * n
+	if math.Abs(denom) < 1e-6 {
+		return nil
+	}
+
+	p0l0 := p.Point.Sub(r.Origin) // p0 - l0
+	t := p0l0.Dot(n) / denom
+	if t < 0 {
+		return nil
+	}
+
+	xMin := p.Point.X - (p.Width / 2)
+	xMax := p.Point.X + (p.Width / 2)
+	zMin := p.Point.Z - (p.Width / 2)
+	zMax := p.Point.Z + (p.Width / 2)
+
+	ir := r.At(t)
+	if ir.X >= xMin && ir.X <= xMax && ir.Z >= zMin && ir.Z <= zMax {
+		return &HitRecord{t, p, p.Material, p.Normal.Normalize()}
+	}
+
+	return nil
+}
+
+func (p Plane) Bounds() Bounds3 {
+	halfWidth := p.Width / 2
+
+	// find two orthogonal vectors on the plane
+	var u, v Vec3
+	if math.Abs(p.Normal.X) > math.Abs(p.Normal.Y) {
+		u = Vec3{-p.Normal.Z, 0, p.Normal.X}
+	} else {
+		u = Vec3{0, -p.Normal.Z, p.Normal.Y}
+	}
+	u = u.Normalize()
+	v = p.Normal.Cross(u)
+	v = v.Normalize()
+
+	// calculate the corners of the plane
+	c1 := Vec3{p.Point.X + halfWidth*u.X + halfWidth*v.X, p.Point.Y + halfWidth*u.Y + halfWidth*v.Y, p.Point.Z + halfWidth*u.Z + halfWidth*v.Z}
+	c2 := Vec3{p.Point.X + halfWidth*u.X - halfWidth*v.X, p.Point.Y + halfWidth*u.Y - halfWidth*v.Y, p.Point.Z + halfWidth*u.Z - halfWidth*v.Z}
+	c3 := Vec3{p.Point.X - halfWidth*u.X + halfWidth*v.X, p.Point.Y - halfWidth*u.Y + halfWidth*v.Y, p.Point.Z - halfWidth*u.Z + halfWidth*v.Z}
+	c4 := Vec3{p.Point.X - halfWidth*u.X - halfWidth*v.X, p.Point.Y - halfWidth*u.Y - halfWidth*v.Y, p.Point.Z - halfWidth*u.Z - halfWidth*v.Z}
+
+	// calculate pMin and max coordinates
+	pMin := Point3{
+		X: math.Min(math.Min(c1.X, c2.X), math.Min(c3.X, c4.X)),
+		Y: math.Min(math.Min(c1.Y, c2.Y), math.Min(c3.Y, c4.Y)),
+		Z: math.Min(math.Min(c1.Z, c2.Z), math.Min(c3.Z, c4.Z)),
+	}
+	pMax := Point3{
+		X: math.Max(math.Max(c1.X, c2.X), math.Max(c3.X, c4.X)),
+		Y: math.Max(math.Max(c1.Y, c2.Y), math.Max(c3.Y, c4.Y)),
+		Z: math.Max(math.Max(c1.Z, c2.Z), math.Max(c3.Z, c4.Z)),
+	}
+
+	return Bounds3{pMin, pMax}
+}
 
 // Triangle
 type Triangle struct {
@@ -108,14 +138,10 @@ type Triangle struct {
 	Material   shading.Material
 }
 
-func (t *Triangle) GetMaterial() shading.Material {
-	return t.Material
-}
-
 func (t *Triangle) Intersect(r Ray) *HitRecord {
 	epsilon := 0.000001
 
-	// Compute vectors for two edges of the triangle
+	// compute vectors for two edges of the triangle
 	edge1 := Vec3{
 		X: t.V1.X - t.V0.X,
 		Y: t.V1.Y - t.V0.Y,
@@ -127,14 +153,14 @@ func (t *Triangle) Intersect(r Ray) *HitRecord {
 		Z: t.V2.Z - t.V0.Z,
 	}
 
-	// Compute determinant to check if ray and t are parallel
+	// compute determinant to check if ray and t are parallel
 	h := r.Direction.Cross(edge2)
 	det := edge1.Dot(h)
 	if math.Abs(det) < epsilon {
 		return nil
 	}
 
-	// Compute inverse determinant and barycentric coordinates
+	// compute inverse determinant and barycentric coordinates
 	invDet := 1.0 / det
 	s := Vec3{
 		X: r.Origin.X - t.V0.X,
@@ -152,7 +178,7 @@ func (t *Triangle) Intersect(r Ray) *HitRecord {
 		return nil
 	}
 
-	// Compute intersection distance
+	// compute intersection distance
 	tr := invDet * edge2.Dot(q)
 
 	// calculate a normal
--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	// construct the scene
 	s := core.Scene{
 		Background:       shading.Color{R: 0.1, G: 0.3, B: 0.3},
-		Camera:           geometry.Vec3{X: 0., Y: 0., Z: -10.},
+		Camera:           geometry.Vec3{X: 0., Y: 0., Z: -50.},
 		AmbientIntensity: shading.Color{R: 0.1, G: 0.1, B: 0.1},
 		Light: &core.Light{
 			Pos:               geometry.Vec3{X: 0., Y: 30., Z: -10.},
@@ -40,38 +40,38 @@ func main() {
 			SpecularIntensity: shading.Color{R: 0.8, G: 0.8, B: 0.8},
 		},
 		Primitives: []geometry.Primitive{
-			/* &geometry.Sphere{
-				Center:   geometry.Vec3{X: 0., Y: 0., Z: 25},
-				R:        R,
-				Material: shading.Glass,
-			},
-			&geometry.Sphere{
-				Center:   geometry.Vec3{X: 70, Y: -40, Z: 10},
-				R:        10,
-				Material: shading.RedRubber,
-			},
-			&geometry.Sphere{
-				Center:   geometry.Vec3{X: -70, Y: -40, Z: 10},
-				R:        10,
-				Material: shading.RedRubber,
-			},
-			&geometry.Sphere{
-				Center:   geometry.Vec3{X: 0, Y: -40, Z: 10},
-				R:        10,
-				Material: shading.Ivory,
-			},
-			&geometry.Triangle{
-				V0:       geometry.Vec3{X: -100.0, Y: 0.0, Z: 0.0},
-				V1:       geometry.Vec3{X: -20.0, Y: 75.0, Z: 50.0},
-				V2:       geometry.Vec3{X: 50.0, Y: 0.0, Z: 100.0},
-				Material: shading.RedRubber,
-			},
-			&geometry.Plane{
-				Width:    250.0,
-				Point:    geometry.Vec3{X: 0, Y: -50, Z: 75},
-				Normal:   geometry.Vec3{X: 0, Y: 1, Z: 0},
-				Material: shading.Glass,
-			}, */
+			// &geometry.Sphere{
+			// 	Center:   geometry.Vec3{X: 0., Y: 0., Z: 25},
+			// 	R:        R,
+			// 	Material: shading.Glass,
+			// },
+			// &geometry.Sphere{
+			// 	Center:   geometry.Vec3{X: 70, Y: -40, Z: 10},
+			// 	R:        10,
+			// 	Material: shading.RedRubber,
+			// },
+			// &geometry.Sphere{
+			// 	Center:   geometry.Vec3{X: -70, Y: -40, Z: 10},
+			// 	R:        10,
+			// 	Material: shading.RedRubber,
+			// },
+			// &geometry.Sphere{
+			// 	Center:   geometry.Vec3{X: 0, Y: -40, Z: 10},
+			// 	R:        10,
+			// 	Material: shading.Ivory,
+			// },
+			// &geometry.Triangle{
+			// 	V0:       geometry.Vec3{X: -100.0, Y: 0.0, Z: 0.0},
+			// 	V1:       geometry.Vec3{X: -20.0, Y: 75.0, Z: 50.0},
+			// 	V2:       geometry.Vec3{X: 50.0, Y: 0.0, Z: 100.0},
+			// 	Material: shading.RedRubber,
+			// },
+			// &geometry.Plane{
+			// 	Width:    250.0,
+			// 	Point:    geometry.Vec3{X: 0, Y: -50, Z: 75},
+			// 	Normal:   geometry.Vec3{X: 0, Y: 1, Z: 0},
+			// 	Material: shading.Glass,
+			// },
 		},
 	}
 
